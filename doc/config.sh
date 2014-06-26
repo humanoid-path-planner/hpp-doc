aa216f325b6507feb81f3c398bf242d497ea2036
Florent Lamiraux <florent@laas.fr>
2014-06-26 11:58:04 +0200

    Update config.sh to hydro.

--- doc/config.sh
+++ doc/config.sh
@@ -1,12 +1,12 @@
 export PATH=$DEVEL_DIR/install/sbin:$DEVEL_DIR/install/bin:$PATH
-export PKG_CONFIG_PATH=$DEVEL_DIR/install/lib/pkgconfig/:/opt/ros/groovy/lib/pkgconfig
+export PKG_CONFIG_PATH=$DEVEL_DIR/install/lib/pkgconfig/:/opt/ros/hydro/lib/pkgconfig
 
-export PYTHONPATH=$DEVEL_DIR/install/lib/python2.7/site-packages:$DEVEL_DIR/install/lib/python2.7/dist-packages:/opt/ros/groovy/lib/python2.7/dist-packages:$PYTHONPATH
+export PYTHONPATH=$DEVEL_DIR/install/lib/python2.7/site-packages:$DEVEL_DIR/install/lib/python2.7/dist-packages:/opt/ros/hydro/lib/python2.7/dist-packages:$PYTHONPATH
 
-export LD_LIBRARY_PATH=$DEVEL_DIR/install/lib:/opt/ros/groovy/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$DEVEL_DIR/install/lib:/opt/ros/hydro/lib:$LD_LIBRARY_PATH
 
 if [ -f $DEVEL_DIR/install/setup.bash ]; then
     source $DEVEL_DIR/install/setup.bash
 else
-    source /opt/ros/groovy/setup.bash
+    source /opt/ros/hydro/setup.bash
 fi
