d59951255f72f573d9fcfdbfd3a5701ccfd1491b
Joseph Mirabel <jmirabel@laas.fr>
2015-10-28 20:50:11 +0100

    Update check-install and auto-install-hpp scripts

--- script/auto-install-hpp.sh
+++ script/auto-install-hpp.sh
@@ -5,25 +5,14 @@ if [ -z ${DEVEL_DIR} ]; then
   export DEVEL_DIR=/local/devel/hpp
 fi
 
-# Add ros repositories
-sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
-
-sudo sh -c 'echo "deb http://archive.ubuntu.com/ubuntu trusty universe" > /etc/apt/sources.list.d/trusty-universe.list'
-
-sudo apt-get update && sudo apt-get --assume-yes upgrade
-
-# Install dependencies
-
-# FIXME: I do not know why this is not in the installed Ubuntu. Is wget missing in Ubuntu 14.04 
-# or is it deboostrap that is not installing the good version ?
-sudo apt-get --assume-yes install wget
-# This is required by OpenSceneGraph and is usually installed with Ubuntu 14.04 and graphic card
-# drivers.
-sudo apt-get --assume-yes install doxygen freeglut3-dev
-
-# From here, it is a standard HPP installation
-sudo apt-get --assume-yes install autoconf g++ cmake libboost-dev liburdfdom-dev libassimp-dev ros-indigo-xacro ros-indigo-kdl-parser ros-indigo-common-msgs ros-indigo-tf ros-indigo-tf-conversions ros-indigo-libccd ros-indigo-octomap ros-indigo-resource-retriever ros-indigo-srdfdom ros-indigo-pr2-robot flex bison asciidoc source-highlight git libomniorb4-dev omniorb-nameserver omniidl omniidl-python libltdl-dev python-matplotlib libtinyxml2-dev liblog4cxx10-dev libltdl-dev
+# standard HPP installation
+sudo apt-get --assume-yes install doxygen autoconf g++ cmake libboost-dev \
+  liburdfdom-dev libassimp-dev ros-indigo-xacro ros-indigo-kdl-parser \
+  ros-indigo-common-msgs ros-indigo-tf ros-indigo-tf-conversions ros-indigo-libccd \
+  ros-indigo-octomap ros-indigo-resource-retriever ros-indigo-srdfdom \
+  ros-indigo-pr2-robot flex bison asciidoc source-highlight git libomniorb4-dev \
+  omniorb-nameserver omniidl omniidl-python libltdl-dev python-matplotlib \
+  libtinyxml2-dev liblog4cxx10-dev libltdl-dev
 sudo apt-get --assume-yes build-dep openscenegraph
 
 # hpp-gui dependencies
@@ -44,4 +33,5 @@ cd $DEVEL_DIR/src
 
 make robot_state_chain_publisher.install
 source ../config.sh
+make doxygen-Release_1_8_10.install
 make hpp-manipulation-corba.install all hpp-gui.install
--- script/check-install.sh
+++ script/check-install.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+DISTRIBUTION=trusty
+
+## Check whether we are inside LAAS.
+DOMAINNAME=`hostname -d`
+if [ ${DOMAINNAME} = "laas.fr" ]; then
+  # Use mirror from LAAS
+  MIRROR="http://kiev.laas.fr/UBUNTU-MIRROR/"
+else
+  MIRROR="http://archive.ubuntu.com/ubuntu/"
+fi
+
 # read arguments
 if [ $# -ne 1 ] && [ $# -ne 2 ]; then
   echo "usage: $0 <install-directory> <bash_script_to_launch>"
@@ -23,10 +34,31 @@ fi
 
 RUN=`basename "${SCRIPT}"`
 
-# install ubuntu
-debootstrap trusty "$DST"
+### Install ubuntu
+debootstrap ${DISTRIBUTION} "$DST" ${MIRROR}
 
-# put the installation script in the fresh install
+### Configure the repositories
+# ROS
+sh -c "echo 'deb http://packages.ros.org/ros/ubuntu ${DISTRIBUTION} main' > ${DST}/etc/apt/sources.list.d/ros-latest.list"
+chroot ${DST} /usr/bin/apt-key adv --keyserver hkp://pool.sks-keyservers.net --recv-key 0xB01FA116
+
+# Ubuntu
+sh -c "echo '# Intentionally empty' > ${DST}/etc/apt/sources.list"
+sh -c "echo 'deb ${MIRROR} ${DISTRIBUTION} main universe' > ${DST}/etc/apt/sources.list.d/${DISTRIBUTION}.list"
+
+# Update
+chroot ${DST} /usr/bin/apt-get update
+chroot ${DST} /usr/bin/apt-get --assume-yes upgrade
+
+# Install dependencies
+# FIXME: I do not know why this is not in the installed Ubuntu. Is wget missing in Ubuntu 14.04 
+# or is it deboostrap that is not installing the good version ?
+chroot ${DST} /usr/bin/apt-get --assume-yes install wget
+# This is required by OpenSceneGraph and is usually installed with Ubuntu 14.04 and graphic card
+# drivers.
+chroot ${DST} /usr/bin/apt-get --assume-yes install freeglut3-dev
+
+### Run the script
 cp "${SCRIPT}" "$DST/root/$RUN"
 
 chroot "${DST}" /bin/bash "/root/$RUN"
