d0d7a7f4f351c07c1cafcd5c475890e8fa06ee55
Florent Lamiraux <florent@laas.fr>
2015-10-29 21:59:08 +0100

    Update README.md and doc/Makefile

--- script/auto-install-hpp.sh
+++ script/auto-install-hpp.sh
@@ -6,7 +6,7 @@ if [ -z ${DEVEL_DIR} ]; then
 fi
 
 # standard HPP installation
-sudo apt-get --assume-yes install doxygen autoconf g++ cmake libboost-dev \
+sudo apt-get --assume-yes install autoconf g++ cmake libboost-dev \
   liburdfdom-dev libassimp-dev ros-indigo-xacro ros-indigo-kdl-parser \
   ros-indigo-common-msgs ros-indigo-tf ros-indigo-tf-conversions ros-indigo-libccd \
   ros-indigo-octomap ros-indigo-resource-retriever ros-indigo-srdfdom \
