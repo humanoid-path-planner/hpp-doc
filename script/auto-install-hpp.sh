1d12b7c2c4c77025e09459bff086be01873d289b
Joseph Mirabel <jmirabel@laas.fr>
2015-12-27 12:25:32 +0100

    Update auto-install-hpp.sh
    * Can generate tarball

--- script/auto-install-hpp.sh
+++ script/auto-install-hpp.sh
@@ -1,22 +1,71 @@
 #!/bin/bash
 
-BRANCH=devel
-if [ -z ${DEVEL_DIR} ]; then
-  export DEVEL_DIR=/local/devel/hpp
-fi
-
-# standard HPP installation
-sudo apt-get --assume-yes install autoconf g++ cmake libboost-dev \
+APT_DEP="autoconf g++ cmake libboost-dev \
   liburdfdom-dev libassimp-dev ros-indigo-xacro ros-indigo-kdl-parser \
   ros-indigo-common-msgs ros-indigo-tf ros-indigo-tf-conversions ros-indigo-libccd \
   ros-indigo-octomap ros-indigo-resource-retriever ros-indigo-srdfdom \
   ros-indigo-pr2-robot flex bison asciidoc source-highlight git libomniorb4-dev \
   omniorb-nameserver omniidl omniidl-python libltdl-dev python-matplotlib \
-  libtinyxml2-dev liblog4cxx10-dev libltdl-dev
-sudo apt-get --assume-yes build-dep openscenegraph
+  libtinyxml2-dev liblog4cxx10-dev libltdl-dev \
+  qt4-dev-tools libqt4-opengl-dev libqtgui4 oxygen-icon-theme"
+APT_BUILD_DEP="openscenegraph"
 
-# hpp-gui dependencies
-sudo apt-get --assume-yes install qt4-dev-tools libqt4-opengl-dev libqtgui4 oxygen-icon-theme
+MAKE_TARBALL=false
+
+BRANCH=devel
+if [ -z ${DEVEL_DIR} ]; then
+  export DEVEL_DIR=/local/devel/hpp
+fi
+if [ -z ${BUILD_TYPE} ]; then
+  export BUILD_TYPE=Release
+fi
+
+while test $# -gt 0
+do
+  case $1 in
+    --mktar)
+      MAKE_TARBALL=true
+      ;;
+    --show-dep)
+      echo "Will install"
+      echo "${APT_DEP}"
+      echo "\nWill install build dependencies of"
+      echo "${APT_BUILD_DEP}"
+      exit 0
+      ;;
+    --help)
+      echo "Options are"
+      echo "--mktar:   \tmake tar balls after compilation"
+      echo "--show-dep:\tshow dependencies resolved by aptitude"
+      echo "-v:        \tshow variables and ask for confirm (must be last arg)"
+      exit 0
+      ;;
+    -v)
+      for v in "DEVEL_DIR" "BUILD_TYPE" "MAKE_TARBALL"
+      do
+        echo "$v=${!v}"
+      done
+      read -p "Continue (y/N)?" choice
+      case "$choice" in 
+        y|Y )
+          echo "yes"
+          ;;
+        * ) echo "no"
+          exit 1
+          ;;
+      esac
+      ;;
+    *)
+      echo "Option $1 not understood. Use --help"
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+# standard HPP installation
+sudo apt-get --assume-yes install ${APT_DEP}
+sudo apt-get --assume-yes build-dep ${APT_BUILD_DEP}
 
 # Setup environment
 mkdir --parents $DEVEL_DIR
@@ -31,7 +80,23 @@ source $DEVEL_DIR/config.sh
 
 cd $DEVEL_DIR/src
 
-make robot_state_chain_publisher.install
+make -e robot_state_chain_publisher.install
 source ../config.sh
-make doxygen-Release_1_8_10.install
-make hpp-manipulation-corba.install all hpp-gui.install
+make -e doxygen-Release_1_8_10.install
+make -e hpp-manipulation-corba.install all hpp-gui.install
+
+if [ ${MAKE_TARBALL} = true ]; then
+  cd $DEVEL_DIR/
+  mkdir tarball
+  SUFFIX="${BRANCH}-`date +%Y%m%d`-${BUILD_TYPE}"
+  tar czf "tarball/hpp.src.${SUFFIX}.tar.gz" src/ install/
+  tar czf "tarball/hpp.${SUFFIX}.tar.gz" install/
+  INSTALL="$DEVEL_DIR/tarball/check.${SUFFIX}.sh"
+  echo "#!/bin/bash" > ${INSTALL}
+  echo "# Dependencies" >> ${INSTALL}
+  echo "sudo apt-get install $APT_DEP" >> ${INSTALL}
+  echo "sudo apt-get build-dep $APT_BUILD_DEP" >> ${INSTALL}
+  echo "" >> ${INSTALL}
+  echo "# config.sh" >> ${INSTALL}
+  echo "echo \"Configure using 'config.sh' file\"" >> ${INSTALL}
+fi
