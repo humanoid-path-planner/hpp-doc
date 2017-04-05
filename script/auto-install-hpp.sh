cc8fc1f0ea0941e38e4c0bb21573c5b6f5976241
Joseph Mirabel <jmirabel@laas.fr>
2017-04-05 13:19:36 +0200

    auto-install-hpp.sh uses correct doxygen target

--- script/auto-install-hpp.sh
+++ script/auto-install-hpp.sh
@@ -82,7 +82,7 @@ cd $DEVEL_DIR/src
 
 make -e robot_state_chain_publisher.install
 source ../config.sh
-make -e doxygen-Release_1_8_10.install
+make -e doxygen-1.8.10.install
 make -e hpp-manipulation-corba.install all hpp-gui.install
 
 if [ ${MAKE_TARBALL} = true ]; then
