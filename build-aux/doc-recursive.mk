3bbdcb27009f4dd6c0c3734dfdea70c1669cc9a4
Joseph Mirabel <jmirabel@laas.fr>
2017-10-02 16:33:59 +0200

    Remove autoconf and old files.

--- build-aux/doc-recursive.mk
+++
@@ -1,6 +0,0 @@
-# Make documentation in ``doc'' directory.
-# Simulate a recursive rule for doc and html targets.
-# This file should be included in your top-level Makefile.am.
-.PHONY: doc html
-doc html:
-	cd doc && $(MAKE) $(AM_MAKEFLAGS) $@
--- build-aux/libtool.mk
+++
@@ -1,5 +0,0 @@
-# Regenerate libtool if required, should be included by the
-# top-level Makefile.am file.
-LIBTOOL_DEPS = @LIBTOOL_DEPS@
-libtool: $(LIBTOOL_DEPS)
-	$(SHELL) ./config.status --recheck
