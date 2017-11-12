23bf76f81e527ad0f2822735dbe72df784ea32ca
Florent Lamiraux <florent@laas.fr>
2017-11-12 10:21:16 +0100

    Fix LD_LIBRARY_PATH in config.sh
      - collada plugin for OpenSceneGraph is installed in lib64 directory.

--- doc/config.sh
+++ doc/config.sh
@@ -3,7 +3,7 @@ export PKG_CONFIG_PATH=$DEVEL_DIR/install/lib/pkgconfig/:/opt/ros/indigo/lib/pkg
 
 export PYTHONPATH=$DEVEL_DIR/install/lib/python2.7/site-packages:$DEVEL_DIR/install/lib/python2.7/dist-packages:/opt/ros/indigo/lib/python2.7/dist-packages:$PYTHONPATH
 
-export LD_LIBRARY_PATH=$DEVEL_DIR/install/lib:/opt/ros/indigo/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$DEVEL_DIR/install/lib:$DEVEL_DIR/install/lib64:/opt/ros/indigo/lib:$LD_LIBRARY_PATH
 
 if [ -f $DEVEL_DIR/install/setup.bash ]; then
     source $DEVEL_DIR/install/setup.bash
