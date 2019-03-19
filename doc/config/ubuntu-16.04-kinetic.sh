7c58a7f5a3698bd96dfa67d13fe7cbec8c8a7e53
Guilhem Saurel <guilhem.saurel@laas.fr>
2019-03-19 13:50:18 +0100

    add robotpkg to ROS_PACKAGE_PATH in config.sh

--- doc/config/ubuntu-16.04-kinetic.sh
+++ doc/config/ubuntu-16.04-kinetic.sh
@@ -1,22 +1,24 @@
 export INSTALL_HPP_DIR=$DEVEL_HPP_DIR/install
+export ROBOTPKG=/opt/openrobots
+export ROS=/opt/ros/kinetic
 
-export PATH=$INSTALL_HPP_DIR/sbin:$INSTALL_HPP_DIR/bin:/opt/ros/kinetic/bin:$PATH
-export PKG_CONFIG_PATH=$INSTALL_HPP_DIR/lib/pkgconfig/:/opt/ros/kinetic/lib/pkgconfig:/opt/openrobots/lib/pkgconfig
+export PATH=$INSTALL_HPP_DIR/sbin:$INSTALL_HPP_DIR/bin:$ROS/bin:$PATH
+export PKG_CONFIG_PATH=$INSTALL_HPP_DIR/lib/pkgconfig/:$ROS/lib/pkgconfig:$ROBOTPKG/lib/pkgconfig
 
-export PYTHONPATH=$INSTALL_HPP_DIR/lib/python2.7/site-packages:$INSTALL_HPP_DIR/lib/python2.7/dist-packages:/opt/ros/kinetic/lib/python2.7/dist-packages:$PYTHONPATH
+export PYTHONPATH=$INSTALL_HPP_DIR/lib/python2.7/site-packages:$INSTALL_HPP_DIR/lib/python2.7/dist-packages:$ROS/lib/python2.7/dist-packages:$PYTHONPATH
 
-export LD_LIBRARY_PATH=$INSTALL_HPP_DIR/lib:$INSTALL_HPP_DIR/lib64:/opt/ros/kinetic/lib:/opt/openrobots/lib:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=$INSTALL_HPP_DIR/lib:$INSTALL_HPP_DIR/lib64:$ROS/lib:$ROBOTPKG/lib:$LD_LIBRARY_PATH
 
 if [ -f $INSTALL_HPP_DIR/setup.bash ]; then
     source $INSTALL_HPP_DIR/setup.bash
 else
-    source /opt/ros/kinetic/setup.bash
+    source $ROS/setup.bash
 fi
 # Make sure that /opt/ros/kinetic/setup.bash is in the ROS_PACKAGE_PATH,
 # otherwise, you should add it by hand in the line below.
-export ROS_PACKAGE_PATH=$INSTALL_HPP_DIR/share:$ROS_PACKAGE_PATH
+export ROS_PACKAGE_PATH=$INSTALL_HPP_DIR/share:$ROBOTPKG/share:$ROS_PACKAGE_PATH
 
 if [ -f "${INSTALL_HPP_DIR}/etc/hpp-tools/bashrc" ]; then
     source "${INSTALL_HPP_DIR}/etc/hpp-tools/bashrc"
 fi
-export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:/opt/openrobots
+export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:$ROBOTPKG
