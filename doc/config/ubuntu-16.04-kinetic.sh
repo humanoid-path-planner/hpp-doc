3f1ba99997e844928951488d3b356d760fe2a433
Florent Lamiraux <florent@laas.fr>
2019-03-26 16:27:58 +0100

    Do not let ROS override PKG_CONFIG_PATH.

--- doc/config/ubuntu-16.04-kinetic.sh
+++ doc/config/ubuntu-16.04-kinetic.sh
@@ -1,7 +1,6 @@
 export INSTALL_HPP_DIR=$DEVEL_HPP_DIR/install
 
 export PATH=$INSTALL_HPP_DIR/sbin:$INSTALL_HPP_DIR/bin:/opt/ros/kinetic/bin:$PATH
-export PKG_CONFIG_PATH=$INSTALL_HPP_DIR/lib/pkgconfig/:/opt/ros/kinetic/lib/pkgconfig:/opt/openrobots/lib/pkgconfig
 export CMAKE_PREFIX_PATH=/opt/openrobots
 
 export PYTHONPATH=$INSTALL_HPP_DIR/lib/python2.7/site-packages:$INSTALL_HPP_DIR/lib/python2.7/dist-packages:/opt/ros/kinetic/lib/python2.7/dist-packages:$PYTHONPATH
@@ -20,3 +19,5 @@ export ROS_PACKAGE_PATH=$INSTALL_HPP_DIR/share:$ROS_PACKAGE_PATH
 if [ -f "${INSTALL_HPP_DIR}/etc/hpp-tools/bashrc" ]; then
     source "${INSTALL_HPP_DIR}/etc/hpp-tools/bashrc"
 fi
+
+export PKG_CONFIG_PATH=$INSTALL_HPP_DIR/lib/pkgconfig/:/opt/ros/kinetic/lib/pkgconfig:/opt/openrobots/lib/pkgconfig
