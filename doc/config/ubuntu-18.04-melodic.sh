4604bd3cd6711a75d74025a6c49693d76b5a03c5
Florent Lamiraux <florent@laas.fr>
2019-12-19 18:00:56 +0100

    [config.sh] do not source ROS setup.sh file.

--- doc/config/ubuntu-18.04-melodic.sh
+++ doc/config/ubuntu-18.04-melodic.sh
@@ -2,23 +2,18 @@ export INSTALL_HPP_DIR=$DEVEL_HPP_DIR/install
 export ROBOTPKG=/opt/openrobots
 export ROS=/opt/ros/melodic
 
-export PATH=$INSTALL_HPP_DIR/sbin:$INSTALL_HPP_DIR/bin:$ROS/bin:$PATH
+export PATH=$INSTALL_HPP_DIR/sbin:$INSTALL_HPP_DIR/bin:$ROBOTPKG/bin:$ROBOTPKG/sbin:$ROS/bin:$PATH
 export PKG_CONFIG_PATH=$INSTALL_HPP_DIR/lib/pkgconfig/:$ROS/lib/pkgconfig:$ROBOTPKG/lib/pkgconfig
 
-export PYTHONPATH=$INSTALL_HPP_DIR/lib/python3/dist-packages:$INSTALL_HPP_DIR/lib/python2.7/site-packages:$INSTALL_HPP_DIR/lib/python2.7/dist-packages:$ROS/lib/python2.7/dist-packages:$ROBOTPKG/lib/python3.6/site-packages:$ROBOTPKG/lib/python2.7/site-packages:$PYTHONPATH
+export PYTHONPATH=$INSTALL_HPP_DIR/lib/python3/dist-packages:$ROBOTPKG/lib/python3.6/site-packages:$INSTALL_HPP_DIR/lib/python2.7/site-packages:$INSTALL_HPP_DIR/lib/python2.7/dist-packages:$ROBOTPKG/lib/python2.7/site-packages:$ROS/lib/python2.7/dist-packages:$PYTHONPATH
 
 export LD_LIBRARY_PATH=$INSTALL_HPP_DIR/lib:$INSTALL_HPP_DIR/lib64:$ROS/lib:$ROBOTPKG/lib:$LD_LIBRARY_PATH
 
-if [ -f $INSTALL_HPP_DIR/setup.bash ]; then
-    source $INSTALL_HPP_DIR/setup.bash
-else
-    source $ROS/setup.bash
-fi
 # Make sure that /opt/ros/melodic is in the ROS_PACKAGE_PATH,
 # otherwise, you should add it by hand in the line below.
-export ROS_PACKAGE_PATH=$INSTALL_HPP_DIR/share:$ROBOTPKG/share:$ROS_PACKAGE_PATH
+export ROS_PACKAGE_PATH=$INSTALL_HPP_DIR/share:$ROBOTPKG/share:$ROS/share
+export CMAKE_PREFIX_PATH=$INSTALL_HPP_DIR:$ROBOTPKG:$ROS:/usr
 
 if [ -f "${INSTALL_HPP_DIR}/etc/hpp-tools/bashrc" ]; then
     source "${INSTALL_HPP_DIR}/etc/hpp-tools/bashrc"
 fi
-export CMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH:$ROBOTPKG:/usr
