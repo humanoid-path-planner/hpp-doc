e24b72017843512eaf0ded2e8e8dd30a5b4218ee
Guilhem Saurel <guilhem.saurel@laas.fr>
2023-01-17 20:54:20 +0100

    remove hpp-rbprm-corba from test-ci

--- makefiles/devel.mk
+++ makefiles/devel.mk
@@ -218,7 +218,7 @@ all: hpp_tutorial.install hpp-gepetto-viewer.install hpp-plot.install hpp-gui.in
 # For test on gepgitlab, install robot packages first
 test-ci: example-robot-data.install  hpp-environments.install \
 	hpp-baxter.install
-	${MAKE} hpp_tutorial.install hpp-gepetto-viewer.install hpp-rbprm-corba.install \
+	${MAKE} hpp_tutorial.install hpp-gepetto-viewer.install hpp-affordance-corba.install hpp-rbprm.install \
 	hpp-universal-robot.install && \
 	${MAKE} hpp-doc.install
 
