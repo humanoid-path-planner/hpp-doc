b86e4015947a85549ec893cfe99c56d11482ff53
Guilhem Saurel <guilhem.saurel@laas.fr>
2026-04-16 12:23:39 +0200

    sync with gepetto/nix template

--- flake.nix
+++ flake.nix
@@ -1,34 +1,24 @@
 {
   description = "Documentation for project Humanoid Path Planner";
 
-  inputs = {
-    gepetto.url = "github:gepetto/nix";
-    flake-parts.follows = "gepetto/flake-parts";
-    systems.follows = "gepetto/systems";
-  };
+  inputs.gepetto.url = "github:gepetto/nix";
 
   outputs =
     inputs:
-    inputs.flake-parts.lib.mkFlake { inherit inputs; } (
+    inputs.gepetto.lib.mkFlakoboros inputs (
       { lib, ... }:
       {
-        systems = import inputs.systems;
-        imports = [
-          inputs.gepetto.flakeModule
-          {
-            flakoboros.overrideAttrs.hpp-doc = _: {
-              src = lib.fileset.toSource {
-                root = ./.;
-                fileset = lib.fileset.unions [
-                  ./CMakeLists.txt
-                  ./doc
-                  ./package.xml
-                  ./scripts
-                ];
-              };
-            };
-          }
-        ];
+        overrideAttrs.hpp-doc = {
+          src = lib.fileset.toSource {
+            root = ./.;
+            fileset = lib.fileset.unions [
+              ./CMakeLists.txt
+              ./doc
+              ./package.xml
+              ./scripts
+            ];
+          };
+        };
       }
     );
 }
